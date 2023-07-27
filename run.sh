9c7ebecfbf0a9d55a488ac8e93a8c0e3ea85adc9
Azzolin <steve.azzolin@\unitn.it>
2023-07-27 14:44:31 +0200

    Changed batch_size + run.sh

---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH -p long-disi
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --job-name=graph_rnn
+#SBATCH -t 6:00:00
+#SBATCH --output=/home/steve.azzolin/GraphRNN_fork/sbatch_outputs/grid.txt
+#SBATCH --error=/home/steve.azzolin/GraphRNN_fork/sbatch_outputs/v.txt
+#SBATCH --ntasks=1
+#SBATCH -N 1
+#SBATCH --mail-user=steve.azzolin@unitn.it
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+start=`date +%s`
+
+set -e
+export PATH="/nfs/data_chaos/sazzolin/miniconda3/bin:$PATH"
+eval "$(conda shell.bash hook)"
+conda activate graphrnn
+
+
+DATASET=sbm
+BIGGER=200
+python main.py 
+
+
+echo DONE
+end=`date +%s`
+runtime=$( echo "$end - $start" | bc -l )
+runtime=$((runtime / 60))
+echo Execution lasted $runtime minutes
